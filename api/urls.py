ee97b12a84c0cec7611d7aa014e53e332b098285
chinazagideon <chinazagideon264@gmail.com>
2025-09-20 02:12:06 +0100

    implement module endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,22 @@
 """
 URL configuration for api app.
 """
-from django.urls import path
-from job.views import *
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+# from .views import ProtectedView
+
 
 urlpatterns = [
-    path('jobs/', JobListCreateView.as_view(), name='job-list-create'),
-    path('jobs/<int:pk>/', JobDetailView.as_view(), name='job-detail'),
+    path('jobs/', include('job.urls')),
+    path('uploads/', include('upload.urls')),
+    path('promotions/', include('promotion.urls')),
+    path('application', include('application.urls')),
+    path('users/', include('user.urls')),
+    path('addresses/', include('address.urls')),
+    path('companies/', include('company.urls')),
+    path('skills/', include('skill.urls')),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('protected/', ProtectedView.as_view(), name='protected'),
 ]
