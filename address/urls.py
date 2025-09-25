108c241198180a224fa471c0a28647122b63153b
chinazagideon <chinazagideon264@gmail.com>
2025-09-25 12:30:04 +0100

    remove redundant routes

--- address/urls.py
+++ address/urls.py
@@ -16,9 +16,9 @@ from .lookup_views import (
 
 router = DefaultRouter()
 router.register(r'addresses', AddressViewSet, basename='address')
-router.register(r'cities', CityViewSet, basename='city')
-router.register(r'states', StateViewSet, basename='state')
-router.register(r'countries', CountryViewSet, basename='country')
+# router.register(r'cities', CityViewSet, basename='city')
+# router.register(r'states', StateViewSet, basename='state')
+# router.register(r'countries', CountryViewSet, basename='country')
 
 urlpatterns = [
     # Lookup endpoints
