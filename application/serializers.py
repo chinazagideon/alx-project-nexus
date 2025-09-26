808a2a45e9db9009dbf67ebb3282c365cf9bae37
chinazagideon <chinazagideon264@gmail.com>
2025-09-27 00:45:00 +0100

    add job details to view

--- application/serializers.py
+++ application/serializers.py
@@ -7,19 +7,25 @@ from rest_framework import serializers
 from user.models.models import User
 from user.models.models import UserRole
 from application.models import Application
+from job.serializers import JobSerializer
+from user.serializers import UserSerializer
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
     """
     Serializer for the application model
     """
+    job_details = JobSerializer(source='job', read_only=True)
+    user_details = UserSerializer(source='user', read_only=True)
 
     class Meta:
         model = Application
         fields = (
             "id",
             "job",
+            "job_details",
             "user", 
+            "user_details",
             "status",
             "date_applied",
             "cover_letter",
